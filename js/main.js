9f88b8f8aabe158794b1a103102e892101a57c43
mehsam1999 <97000101+mehsam1999@users.noreply.github.com>
2022-01-10 20:03:46 +0600

    hello

--- js/main.js
+++ js/main.js
@@ -186,7 +186,6 @@
 	};
 	contentWayPoint();
 
-    newFunction();
 
 	
 	// magnific popup
@@ -223,24 +222,53 @@
   $('[data-toggle="popover"]').popover()
 	$('[data-toggle="tooltip"]').tooltip()
 
+
 })(jQuery);
 
-function newFunction() {
-	function show() {
-		var name = document.getElementById("name").value;
-		var message = document.getElementById("message").value;
 
-		firebase.database().ref('User/' + name).set({
-			name: name,
-			message: message
-		}, function (error) {
-			if (error) {
-				//The write failed
-			} else {
-				alert("DONE");
-			}
+function show() {
+	var name = document.getElementById("name").value;
+	var email=document.getElementById("email").value;
+	var message = document.getElementById("message").value;
 
-		});
-	}
+	firebase.database().ref('User/' + name).set({
+		name: name,
+		email : email,
+		message: message,
+	}, function (error) {
+		if (error) {
+			//The write failed
+		} else {
+			alert("DONE");
+		}
+
+	});
 }
+
+function search() {
+	var name=document.getElementById("name1").value;
+
+	firebase.database().ref('User/' + name).once('value').then(function(snapshot){
+  if(snapshot.exists()) {
+	var name_=snapshot.val().name;
+	var mail_=snapshot.val().email;
+	var message_=snapshot.val().message;
+	document.getElementById("name").value=name_;
+	document.getElementById("email").value=mail_;
+	document.getElementById("message").value=message_;
+  }
+  else{
+
+  }
+}, function(error) {
+  if(error){
+
+  } else{
+
+  }
+});
+	  }
+
+
+
 
